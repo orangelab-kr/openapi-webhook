deb3d3eb5640c937a5337a261d05b8888ccc62da
bakjonghun <bakjonghun@hikick.kr>
2021-04-13 17:17:20 +0900

    add: logger & opcode & wrapper

--- src/tools/wrapper.ts
+++ src/tools/wrapper.ts
@@ -1,16 +1,17 @@
+import * as Sentry from '@sentry/node';
+
 import { NextFunction, Request, Response } from 'express';
+import { OPCODE, logger } from '.';
 
-import InternalError from './error';
-import OPCODE from './opcode';
-import logger from './logger';
+import { ValidationError } from 'joi';
 
-type Callback = (
+export type Callback = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => Promise<unknown>;
 
-export default function Wrapper(cb: Callback): Callback {
+export function Wrapper(cb: Callback): Callback {
   return async function (req: Request, res: Response, next: NextFunction) {
     try {
       return await cb(req, res, next);
@@ -21,16 +22,28 @@ export default function Wrapper(cb: Callback): Callback {
       }
 
       let status = 500;
+      let opcode = OPCODE.ERROR;
       let message = '알 수 없는 오류가 발생했습니다.';
+      const eventId = Sentry.captureException(err);
+      let details;
 
-      if (err instanceof InternalError) {
-        status = err.status;
+      if (err.name === 'InternalError') {
+        opcode = err.opcode;
         message = err.message;
+        details = err.details;
+      }
+
+      if (err instanceof ValidationError) {
+        status = 400;
+        message = '올바른 정보를 입력해주세요.';
+        details = err.details;
       }
 
       res.status(status).json({
-        opcode: OPCODE.ERROR,
+        opcode,
+        eventId,
         message,
+        details,
       });
     }
   };
